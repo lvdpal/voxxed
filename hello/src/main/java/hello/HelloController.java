ce11e01e1c30ec25010f0909e622176fc766534b
reginatb <reginatb@jduchess.org>
2018-10-24 20:37:21 +0200

    Basic Hello Spring Boot Application

---
+++ hello/src/main/java/hello/HelloController.java
@@ -0,0 +1,12 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String getIndex() {
+        return "Hello Voxxed Microservices";
+    }
+}
