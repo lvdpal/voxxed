af3cf6c468f2fe644f436b041e66ca250ac9c23f
reginatb <reginatb@jduchess.org>
2018-10-27 21:33:59 +0200

    ticketservice nu met url's en andere annotaties

--- ticketService/src/main/java/org/jduchess/microservices/TicketController.java
+++ ticketService/src/main/java/org/jduchess/microservices/TicketController.java
@@ -3,26 +3,40 @@ package org.jduchess.microservices;
 import org.jduchess.microservices.model.Event;
 import org.jduchess.microservices.model.Ticket;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @RestController
 public class TicketController {
 
-
-    @Autowired
     private TicketRepository ticketRepository;
 
-    public void updateTicket(Ticket ticket) {
+    public TicketController(@NotNull TicketRepository ticketRepository) {
+        this.ticketRepository = ticketRepository;
+    }
+
+    @RequestMapping(value = "/updateTicket", method = RequestMethod.POST)
+    public void updateTicket(@RequestBody Ticket ticket) {
         ticketRepository.save(ticket);
     }
 
-    public void updateTickets(List<Ticket> tickets) {
+    @RequestMapping(value = "/updateTickets", method = RequestMethod.POST)
+    public void updateTickets(@RequestBody List<Ticket> tickets) {
         ticketRepository.saveAll(tickets);
     }
 
-    public List<Ticket> getTicketsForEvent(Event event) {
+    @RequestMapping(value = "/ticketsForEvent", method = RequestMethod.POST)
+    public List<Ticket> getTicketsForEvent(@RequestBody Event event) {
         return ticketRepository.findByEvent(event);
+    }
+
+    @RequestMapping(value = "/addTickets", method = RequestMethod.POST)
+    public void addTickets(@RequestBody List<Ticket> tickets) {
+        ticketRepository.saveAll(tickets);
     }
 }
