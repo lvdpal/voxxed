bd8e2f9234c436d13d2baff08f38d4773cb519bf
reginatb <reginatb@jduchess.org>
2018-10-27 18:26:47 +0200

    ticketservice wat methodes gegeven

--- ticketService/src/main/java/org/jduchess/microservices/TicketController.java
+++ ticketService/src/main/java/org/jduchess/microservices/TicketController.java
@@ -1,8 +1,28 @@
 package org.jduchess.microservices;
 
+import org.jduchess.microservices.model.Event;
+import org.jduchess.microservices.model.Ticket;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TicketController {
+
+
+    @Autowired
     private TicketRepository ticketRepository;
+
+    public void updateTicket(Ticket ticket) {
+        ticketRepository.save(ticket);
+    }
+
+    public void updateTickets(List<Ticket> tickets) {
+        ticketRepository.saveAll(tickets);
+    }
+
+    public List<Ticket> getTicketsForEvent(Event event) {
+        return ticketRepository.findByEvent(event);
+    }
 }
--- ticketService/src/main/java/org/jduchess/microservices/TicketRepository.java
+++ ticketService/src/main/java/org/jduchess/microservices/TicketRepository.java
@@ -1,7 +1,12 @@
 package org.jduchess.microservices;
 
+import org.jduchess.microservices.model.Event;
 import org.springframework.data.repository.CrudRepository;
 import org.jduchess.microservices.model.Ticket;
 
+import java.util.List;
+
 public interface TicketRepository extends CrudRepository<Ticket, Integer> {
+
+    List<Ticket> findByEvent(Event event);
 }
