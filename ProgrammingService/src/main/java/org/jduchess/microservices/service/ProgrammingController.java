00e5cd1ec0ef081c8dd9a2aefb7e9e20aa4ff659
reginatb <reginatb@jduchess.org>
2018-10-30 10:31:12 +0100

    CrossOrigin and correct mappings for programmingService and missing slash for marketingService

--- ProgrammingService/src/main/java/org/jduchess/microservices/service/ProgrammingController.java
+++ ProgrammingService/src/main/java/org/jduchess/microservices/service/ProgrammingController.java
@@ -3,14 +3,12 @@ package org.jduchess.microservices.service;
 import org.jduchess.microservices.domain.Event;
 import org.jduchess.microservices.domain.EventSeating;
 import org.jduchess.microservices.domain.Location;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/programming")
 public class ProgrammingController {
     private LocationRepository locationRepository;
     private EventRepository eventRepository;
@@ -23,16 +21,19 @@ public class ProgrammingController {
         this.eventSeatingRepository = eventSeatingRepository;
     }
 
+    @CrossOrigin
     @RequestMapping(value = "/getLocations", method = RequestMethod.GET)
     public List<Location> getLocations() {
         return (List<Location>) locationRepository.findAll();
     }
 
+    @CrossOrigin
     @RequestMapping(value = "/addEvent", method = RequestMethod.POST)
     public Event addEvent(@RequestBody Event event) {
         return eventRepository.save(event);
     }
 
+    @CrossOrigin
     @RequestMapping(value = "/addSeating", method = RequestMethod.POST)
     public EventSeating addSeating(@RequestBody EventSeating seating) {
         return eventSeatingRepository.save(seating);
--- marketingService/src/main/java/org/jduchess/microservices/service/MarketingController.java
+++ marketingService/src/main/java/org/jduchess/microservices/service/MarketingController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("marketing")
+@RequestMapping("/marketing")
 public class MarketingController {
     private EventRepository eventRepository;
     private EventDetailRepository eventDetailRepository;
