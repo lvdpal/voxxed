303055bbb1f85f4fd2a372cbdd05aa2dfc94c604
reginatb <reginatb@jduchess.org>
2018-10-30 10:05:25 +0100

    Applications will search in the right location for the entities

--- marketingService/src/main/java/org/jduchess/microservices/service/MarketingServiceApplication.java
+++ marketingService/src/main/java/org/jduchess/microservices/service/MarketingServiceApplication.java
@@ -2,8 +2,10 @@ package org.jduchess.microservices.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "org.jduchess.microservices.domain")
 public class MarketingServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MarketingServiceApplication.class);
